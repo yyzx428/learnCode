0c8e35b9641cc275825ca3fb050327c13f54cc7c
yyzx <zhaokun.yin@gmail.com>
2018-12-13 09:53:21 +0800

    Spring提供的类型绑定方法

---
+++ basic/src/main/java/frameworkUse/Spring/Reflection/BeanWrapperImpl/Test.java
@@ -0,0 +1,23 @@
+package frameworkUse.Spring.Reflection.BeanWrapperImpl;
+
+import org.springframework.beans.BeanWrapperImpl;
+
+public class Test {
+    public static void main(String[] args) {
+        BeanWrapperImpl wrapper = new BeanWrapperImpl(Template.class);
+        wrapper.setPropertyValue("name", "张三");
+        System.out.println(wrapper.getPropertyValue("name"));
+    }
+
+    static class Template {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
---
+++ basic/src/main/java/frameworkUse/Spring/Reflection/DataBinder/Test.java
@@ -0,0 +1,51 @@
+package frameworkUse.Spring.Reflection.DataBinder;
+
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.validation.DataBinder;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        Template template = new Template();
+        DataBinder binder = new DataBinder(template);
+        binder.setConversionService(new DefaultFormattingConversionService());
+        MutablePropertyValues ps = new MutablePropertyValues();
+        ps.add("name", "测试模板");
+        ps.add("nums[0]", "1");
+        ps.add("nums[1]", "2");
+        binder.bind(ps);
+        System.out.println(template);
+    }
+
+
+    static class Template {
+        private String name;
+        private List<Integer> nums;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<Integer> getNums() {
+            return nums;
+        }
+
+        public void setNums(List<Integer> nums) {
+            this.nums = nums;
+        }
+
+        @Override
+        public String toString() {
+            return "Template{" +
+                    "name='" + name + '\'' +
+                    ", nums=" + nums +
+                    '}';
+        }
+    }
+}
