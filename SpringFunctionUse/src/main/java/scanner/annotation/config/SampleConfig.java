a87a1bd0c337830baa133096460f7a00738612d1
yyzx <zhaokun.yin@gmail.com>
2018-12-19 14:26:29 +0800

    Spring 提供的构建上下文的类

--- SpringFunctionUse/src/main/java/scanner/annotation/config/SampleConfig.java
+++ SpringFunctionUse/src/main/java/scanner/annotation/config/SampleConfig.java
@@ -1,7 +1,12 @@
 package scanner.annotation.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SampleConfig {
+    @Bean
+    public Integer numOne(){
+        return 0;
+    }
 }
