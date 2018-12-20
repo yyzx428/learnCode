f92fa34d1125da7647a5cfadac2dc85d7bdc5688
yyzx <zhaokun.yin@gmail.com>
2018-12-20 10:47:02 +0800

    Spring Bean初始化中的PostConstruct、afterPropertiesSet、afterSingletonsInstantiated的执行顺序

--- SpringFunctionUse/src/main/java/scanner/annotation/domain/CycleEntity.java
+++ SpringFunctionUse/src/main/java/scanner/annotation/domain/CycleEntity.java
@@ -1,14 +1,33 @@
 package scanner.annotation.domain;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class CycleEntity {
+import javax.annotation.PostConstruct;
+
+public class CycleEntity implements InitializingBean,SmartInitializingSingleton {
     @Autowired
     private CycleEntity cycleEntity;
     private String name;
 
     public CycleEntity(String name) {
         this.name = name;
+    }
+
+    @PostConstruct
+    public void after(){
+        System.out.println("CycleEntity初始化完成----1");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("CycleEntity初始化完成----2");
+    }
+
+    @Override
+    public void afterSingletonsInstantiated() {
+        System.out.println("所有的Bean初始化完成-----3");
     }
 
     public String getName() {
@@ -22,4 +41,6 @@ public class CycleEntity {
     public CycleEntity getCycleEntity() {
         return cycleEntity;
     }
+
+
 }
